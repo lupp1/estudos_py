40073ca435b0d143e989f83397870c4fed12c15f
Gustavo <35385249+lupp1@users.noreply.github.com>
2020-03-27 10:27:05 -0300

    Update ex017.py

--- ex017.py
+++ ex017.py
@@ -1,4 +1,4 @@
 from math import sqrt, pow
-c1 = float(input('Digite o valor de um cateto: '))
-c2 = float(input('Digite o valor do outro cateto: '))
-print(f'O valor da hipotenusa é {sqrt((pow(c1, 2) + pow(c2, 2)))}')
+catetoOp = float(input('Digite o valor de um cateto: '))
+catetoAdj = float(input('Digite o valor do outro cateto: '))
+print(f'O valor da hipotenusa é {sqrt((pow(catetoOp, 2) + pow(catetoAdj, 2)))}')
